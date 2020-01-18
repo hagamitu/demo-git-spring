e7b9e676f3a8352d4e1a6d848028eed675f396d7
hagamitu <56866953+hagamitu@users.noreply.github.com>
2020-01-18 10:24:11 +0200

    update(TestConroller): add line 3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,5 +16,6 @@ public class DemoApplication {
         System.out.println(y);
         System.out.println(String.format("1:%d", y));
         System.out.println(String.format("02:%d", y));
+        System.out.println(String.format("03:%d", y));
     }
 }
